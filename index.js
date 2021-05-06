afb3c2df31df884feac1f1c3fd3512a3964cd8ef
MMYUGESH <mmyugesh@gmail.com>
2021-05-06 17:28:27 +0530

    second commit

--- index.js
+++ index.js
@@ -83,18 +83,12 @@ new TypeIt("#type2", {
 
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
 gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
-gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".banner", { opacity: 0, duration: 1, delay: 1.5, x: -20 });
 gsap.from(".hero h3", { opacity: 0, duration: 1, delay: 2, y: -50 });
 gsap.from(".hero h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
 gsap.from(".hero h4", { opacity: 0, duration: 1, delay: 3, y: -30 });
 gsap.from(".hero a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
-gsap.from(".nav-item", {
-    opacity: 0,
-    duration: 1,
-    delay: 1.2,
-    y: 30,
-    stagger: 0.2,
-});
+gsap.from(".nav-item", { opacity: 0, duration: 1, delay: 1.2, y: 30, stagger: 0.2, });
 gsap.from(".icons span", {
     opacity: 0,
     duration: 1,
